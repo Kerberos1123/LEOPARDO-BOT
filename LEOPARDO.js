fe12a4a539675994652fb47b853a9b0da8a83e8f
Daniel <115358649+Kerberos1123@users.noreply.github.com>
2025-01-23 20:35:10 -0600

    Update LEOPARDO.js

--- LEOPARDO.js
+++ LEOPARDO.js
@@ -1,5 +1,5 @@
 
-//Con el bot de "Wall" como inspiracion decidi que el clon al tener poca vida y no podía usar la función "disappear" pensé que funcionaría mejor como señuelo, ya que al seguir la misma ruta que el original
+//Con el bot de "Wall" como inspiracion decidi que el clon al tener poca vida y no poder usar la función "disappear" pensé que funcionaría mejor como señuelo, ya que al seguir la misma ruta que el original
 //terminaba atrayendo los ataques hacia él.
 
 //La estrategia del bot/tanque "LEOPARDO" es que mientras el clon distrae al enemigo, el original se posiciona y da vueltas por el borde de la arena, siempre mirando hacia el centro asi tiene
